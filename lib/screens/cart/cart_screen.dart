4055601ea565ef3a74e732f473676606e922b191
ezaatama <ezatama33@gmail.com>
2023-08-25 11:03:28 +0700

    cart screen

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:katarasa/data/cart/cart_cubit.dart';
 import 'package:katarasa/data/cart_item/cart_item_cubit.dart';
 import 'package:katarasa/models/cart_models.dart';
 import 'package:katarasa/models/product_models.dart';
@@ -31,14 +30,35 @@ class _CartScreenState extends State<CartScreen> {
             icon: const Icon(
               Icons.arrow_back_outlined,
               color: ColorUI.BLACK,
-              size: 20,
+              size: 22,
             )),
-        title: Text(
-          "Pesanan Kamu",
-          style: BLACK_TEXT_STYLE.copyWith(
-              fontSize: 20, fontWeight: FontUI.WEIGHT_SEMI_BOLD),
+        title: Row(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width * .050,
+              height: MediaQuery.of(context).size.height * .025,
+              decoration: BoxDecoration(
+                  border: Border.all(color: ColorUI.MEDIUM_BROWN, width: 3),
+                  borderRadius: BorderRadius.circular(100)),
+            ),
+            const SizedBox(width: 15),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Pickup",
+                  style: BLACK_TEXT_STYLE.copyWith(
+                      fontSize: 16, fontWeight: FontUI.WEIGHT_BOLD),
+                ),
+                Text(
+                  "Pesan dan ambil di outlet",
+                  style: BLACK_TEXT_STYLE.copyWith(
+                      fontSize: 14, fontWeight: FontUI.WEIGHT_LIGHT),
+                )
+              ],
+            )
+          ],
         ),
-        centerTitle: true,
       ),
       body: SafeArea(
           child: Stack(
@@ -53,34 +73,75 @@ class _CartScreenState extends State<CartScreen> {
                     if (state is CartItemUpdated) {
                       return SizedBox(
                         height: MediaQuery.of(context).size.height * .700,
-                        child: ListView.builder(
-                            shrinkWrap: true,
-                            itemCount: state.cartItems.length,
-                            itemBuilder: (context, index) {
-                              CartItem product = state.cartItems[index];
-                              CartItem cartItem = state.cartItems.firstWhere(
-                                  (item) =>
-                                      item.product.id == product.product.id,
-                                  orElse: () => CartItem(
-                                      product: product.product, quantity: 1));
-                              // print(cartItem.quantity);
-                              return CartProductItem(
-                                product: state.cartItems[index],
-                                decrementItem: () {
-                                  context
-                                      .read<CartItemCubit>()
-                                      .decrementCartItem(product);
-                                  debugPrint('decrement');
-                                },
-                                quantityItem: cartItem.quantity.toString(),
-                                incrementItem: () {
-                                  context
-                                      .read<CartItemCubit>()
-                                      .incrementCartItem(product);
-                                  debugPrint('increment');
-                                },
-                              );
-                            }),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            ListView.builder(
+                                shrinkWrap: true,
+                                itemCount: state.cartItems.length,
+                                itemBuilder: (context, index) {
+                                  CartItem product = state.cartItems[index];
+                                  CartItem cartItem = state.cartItems
+                                      .firstWhere(
+                                          (item) =>
+                                              item.product.id ==
+                                              product.product.id,
+                                          orElse: () => CartItem(
+                                              product: product.product,
+                                              quantity: 1));
+                                  // print(cartItem.quantity);
+                                  return CartProductItem(
+                                    product: state.cartItems[index],
+                                    decrementItem: () {
+                                      context
+                                          .read<CartItemCubit>()
+                                          .decrementCartItem(product);
+                                      debugPrint('decrement');
+                                    },
+                                    quantityItem: cartItem.quantity.toString(),
+                                    incrementItem: () {
+                                      context
+                                          .read<CartItemCubit>()
+                                          .incrementCartItem(product);
+                                      debugPrint('increment');
+                                    },
+                                    clearItem: () {
+                                      context
+                                          .read<CartItemCubit>()
+                                          .clearItem(product);
+                                      debugPrint('delete');
+                                    },
+                                  );
+                                }),
+                            InkWell(
+                              onTap: () {
+                                Navigator.pushReplacementNamed(
+                                    context, '/home');
+                              },
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                children: [
+                                  const Icon(Icons.arrow_back_ios_new_rounded,
+                                      color: ColorUI.BROWN, size: 18),
+                                  Text(
+                                    "Tambah Pesanan",
+                                    style: BROWN_TEXT_STYLE.copyWith(
+                                        fontSize: 16,
+                                        fontWeight: FontUI.WEIGHT_SEMI_BOLD),
+                                  )
+                                ],
+                              ),
+                            ),
+                            const SizedBox(height: 10),
+                            const Divider(thickness: 2, height: 2),
+                            const SizedBox(height: 10),
+                            Text(
+                              "*Kami buka mulai dari pukul 11.00 - 19.00 WIB",
+                              style: BLACK_TEXT_STYLE.copyWith(
+                                  fontWeight: FontUI.WEIGHT_MEDIUM),
+                            )
+                          ],
+                        ),
                       );
                     }
                     return const Center(child: CircularProgressIndicator());
