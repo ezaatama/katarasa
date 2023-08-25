b2ba3484def1f1f039b1eca64a0cb6295161c146
ezaatama <ezatama33@gmail.com>
2023-08-25 11:03:12 +0700

    cart product widget

--- lib/widgets/cart_product.dart
+++ lib/widgets/cart_product.dart
@@ -9,88 +9,117 @@ class CartProductItem extends StatelessWidget {
       required this.product,
       required this.decrementItem,
       required this.quantityItem,
-      required this.incrementItem});
+      required this.incrementItem,
+      required this.clearItem});
 
   final CartItem product;
   final Function() decrementItem;
   final String quantityItem;
   final Function() incrementItem;
+  final Function() clearItem;
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: MediaQuery.of(context).size.width,
-      margin: const EdgeInsets.only(bottom: 15),
-      child: Row(
+      margin: const EdgeInsets.only(bottom: 20),
+      child: Column(
         children: [
-          ClipRRect(
-            borderRadius: BorderRadius.circular(10),
-            child: Image.asset(
-              product.product.image,
-              fit: BoxFit.cover,
-              width: MediaQuery.of(context).size.width * .200,
-              height: MediaQuery.of(context).size.height * .100,
-            ),
-          ),
-          const SizedBox(width: 10),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  product.product.title,
-                  style: BLACK_TEXT_STYLE.copyWith(
-                      fontSize: 18, fontWeight: FontUI.WEIGHT_SEMI_BOLD),
-                ),
-                product.product.discount == null
-                    ? Text(
-                        'Rp ${product.product.price.toRupiah()}',
-                        style: BLACK_TEXT_STYLE.copyWith(
-                            fontWeight: FontUI.WEIGHT_LIGHT),
-                      )
-                    : Text(
-                        'Rp ${product.product.discount!.toRupiah()}',
-                        style: BLACK_TEXT_STYLE.copyWith(
-                            fontWeight: FontUI.WEIGHT_LIGHT),
-                      )
-              ],
-            ),
-          ),
-          const SizedBox(width: 10),
           Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              quantityItem == '1'
-                  ? CircleAvatar(
-                      radius: 20,
-                      backgroundColor: ColorUI.BACKGROUND_COLOR,
-                      child: IconButton(
-                          onPressed: () {},
-                          icon: const Icon(Icons.remove,
-                              color: ColorUI.WHITE, size: 20)),
-                    )
-                  : CircleAvatar(
-                      radius: 20,
-                      backgroundColor: ColorUI.MEDIUM_BROWN,
-                      child: IconButton(
-                          onPressed: decrementItem,
-                          icon: const Icon(Icons.remove,
-                              color: ColorUI.WHITE, size: 20)),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Image.asset(
+                  product.product.image,
+                  fit: BoxFit.cover,
+                  width: MediaQuery.of(context).size.width * .200,
+                  height: MediaQuery.of(context).size.height * .100,
+                ),
+              ),
+              const SizedBox(width: 10),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      product.product.title,
+                      style: BLACK_TEXT_STYLE.copyWith(
+                          fontSize: 18, fontWeight: FontUI.WEIGHT_SEMI_BOLD),
                     ),
-              const SizedBox(width: 5),
-              Text(
-                quantityItem,
-                style: BLACK_TEXT_STYLE.copyWith(
-                    fontSize: 20, fontWeight: FontUI.WEIGHT_SEMI_BOLD),
+                    Text(
+                      product.product.ingredient,
+                      style: BLACK_TEXT_STYLE.copyWith(
+                          fontSize: 14, fontWeight: FontUI.WEIGHT_LIGHT),
+                    ),
+                  ],
+                ),
               ),
-              const SizedBox(width: 5),
-              CircleAvatar(
-                radius: 20,
-                backgroundColor: ColorUI.MEDIUM_BROWN,
-                child: IconButton(
-                    onPressed: incrementItem,
-                    icon:
-                        const Icon(Icons.add, color: ColorUI.WHITE, size: 20)),
-              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  product.product.discount == null
+                      ? Text(
+                          'Rp ${product.product.price.toRupiah()}',
+                          style: BLACK_TEXT_STYLE.copyWith(
+                              fontSize: 16, fontWeight: FontUI.WEIGHT_LIGHT),
+                        )
+                      : Text(
+                          'Rp ${product.product.discount!.toRupiah()}',
+                          style: BLACK_TEXT_STYLE.copyWith(
+                              fontSize: 16, fontWeight: FontUI.WEIGHT_LIGHT),
+                        ),
+                  const SizedBox(height: 5),
+                  Text(
+                    "x$quantityItem",
+                    style: BLACK_TEXT_STYLE.copyWith(
+                        fontSize: 16, fontWeight: FontUI.WEIGHT_MEDIUM),
+                  ),
+                  const SizedBox(height: 15),
+                  Row(
+                    children: [
+                      InkWell(
+                        onTap: clearItem,
+                        child: Image.asset("assets/icons/trash.png",
+                            width: 25, height: 25),
+                      ),
+                      const SizedBox(width: 10),
+                      quantityItem == '1'
+                          ? CircleAvatar(
+                              radius: 15,
+                              backgroundColor: ColorUI.BACKGROUND_COLOR,
+                              child: IconButton(
+                                  onPressed: () {},
+                                  icon: const Icon(Icons.remove,
+                                      color: ColorUI.WHITE, size: 10)),
+                            )
+                          : CircleAvatar(
+                              radius: 15,
+                              backgroundColor: ColorUI.MEDIUM_BROWN,
+                              child: IconButton(
+                                  onPressed: decrementItem,
+                                  icon: const Icon(Icons.remove,
+                                      color: ColorUI.WHITE, size: 10)),
+                            ),
+                      const SizedBox(width: 5),
+                      Text(
+                        quantityItem,
+                        style: BLACK_TEXT_STYLE.copyWith(
+                            fontSize: 20, fontWeight: FontUI.WEIGHT_SEMI_BOLD),
+                      ),
+                      const SizedBox(width: 5),
+                      CircleAvatar(
+                        radius: 15,
+                        backgroundColor: ColorUI.MEDIUM_BROWN,
+                        child: IconButton(
+                            onPressed: incrementItem,
+                            icon: const Icon(Icons.add,
+                                color: ColorUI.WHITE, size: 10)),
+                      ),
+                    ],
+                  )
+                ],
+              )
             ],
           ),
         ],
