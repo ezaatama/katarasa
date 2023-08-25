8d43102d76b1a9c327cf0b4af83c42d07eac3445
ezaatama <ezatama33@gmail.com>
2023-08-25 11:04:53 +0700

    favorite product widget

--- lib/widgets/favorite_product.dart
+++ lib/widgets/favorite_product.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:katarasa/data/product/product_cubit.dart';
+import 'package:katarasa/utils/constant.dart';
 import 'package:katarasa/widgets/favorite_product_item.dart';
 
 class FavoriteProduct extends StatefulWidget {
@@ -14,20 +15,37 @@ class _FavoriteProductState extends State<FavoriteProduct> {
   @override
   Widget build(BuildContext context) {
     final products = context.read<ProductCubit>().favoriteProduct();
-    return SizedBox(
-      child: GridView.builder(
-          physics: const NeverScrollableScrollPhysics(),
-          padding: const EdgeInsets.all(5.0),
-          itemCount: products.length,
-          shrinkWrap: true,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-              crossAxisCount: 2,
-              childAspectRatio: 1 / 1.6,
-              crossAxisSpacing: 20,
-              mainAxisSpacing: 20),
-          itemBuilder: (context, index) {
-            return FavoriteProductItem(products: products[index]);
-          }),
-    );
+    return products.isNotEmpty
+        ? SizedBox(
+            child: GridView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                padding: const EdgeInsets.all(5.0),
+                itemCount: products.length,
+                shrinkWrap: true,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio: 1 / 1.35,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 20),
+                itemBuilder: (context, index) {
+                  return FavoriteProductItem(products: products[index]);
+                }),
+          )
+        : Container(
+            margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 12),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset("assets/icons/like_product.png", fit: BoxFit.cover),
+                Text(
+                  "Kamu belum menambahkan produk favorite",
+                  textAlign: TextAlign.center,
+                  style: BLACK_TEXT_STYLE.copyWith(
+                      fontSize: 20, fontWeight: FontUI.WEIGHT_SEMI_BOLD),
+                )
+              ],
+            ),
+          );
   }
 }
