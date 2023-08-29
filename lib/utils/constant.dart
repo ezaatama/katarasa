69355692929de39e14266da036f55675b49cd509
ezaatama <ezatama33@gmail.com>
2023-08-29 08:13:36 +0700

    update constant

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -1,6 +1,11 @@
 // ignore_for_file: non_constant_identifier_names, constant_identifier_names
 
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
+class EnvValue {
+  static String get SECRET_API => dotenv.get("SECRET_API");
+}
 
 class ColorUI {
   static const Color BACKGROUND_COLOR = Color(0xFFDAE5EC);
@@ -65,3 +70,20 @@ LinearGradient gradientColor() {
     ],
   );
 }
+
+// method
+const String GET_METHOD = 'GET';
+const String POST_METHOD = 'POST';
+const String PATCH_METHOD = 'PATCH';
+const String PUT_METHOD = 'PUT';
+const String DELETE_METHOD = 'DELETE';
+
+// status code
+const int RESPONSE_OK = 200;
+const int RESPONSE_OK_NO_CONTENT = 204;
+const int RESPONSE_BAD_REQUEST = 400;
+const int RESPONSE_UNAUTHORIZED = 401;
+const int RESPONSE_FORBIDDEN_ACCESS = 403;
+const int RESPONSE_NOT_FOUND = 404;
+const int RESPONSE_VALIDATION = 422;
+const int RESPONSE_SERVER_ERROR = 500;
