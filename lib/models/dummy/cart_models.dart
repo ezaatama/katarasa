7546c667fd262e7e1e86247d1c02fb52b2db9a62
ezaatama <ezatama33@gmail.com>
2023-08-31 10:05:45 +0700

    update models

---
+++ lib/models/dummy/cart_models.dart
@@ -0,0 +1,31 @@
+import 'package:katarasa/models/dummy/product_models.dart';
+
+class CartItem {
+  final ProductModels product;
+  int quantity;
+
+  CartItem({
+    required this.product,
+    this.quantity = 1,
+  });
+
+  CartItem copyWith({
+    ProductModels? product,
+    int? quantity,
+  }) {
+    return CartItem(
+        product: product ?? this.product, quantity: quantity ?? this.quantity);
+  }
+
+  void incrementQuantity() {
+    quantity++;
+  }
+
+  void decrementQuantity() {
+    if (quantity > 1) {
+      quantity--;
+    }
+  }
+}
+
+String methodBank = '';
