63cc08e78b5631bbd241689739cded7ac78e902e
ezaatama <ezatama33@gmail.com>
2023-08-31 10:03:30 +0700

    update dummy product cubit

--- lib/data/dummy/product/product_cubit.dart
+++ lib/data/dummy/product/product_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
-import 'package:katarasa/models/cart_models.dart';
-import 'package:katarasa/models/product_models.dart';
+import 'package:katarasa/models/dummy/cart_models.dart';
+import 'package:katarasa/models/dummy/product_models.dart';
+import 'package:katarasa/models/dummy/product_models.dart';
 import 'package:meta/meta.dart';
 
 part 'product_state.dart';
