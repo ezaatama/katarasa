1496e1627fc5c65ed1ba97c642bc32ca81693f5e
ezaatama <ezatama33@gmail.com>
2023-08-31 10:10:52 +0700

    update product

--- lib/widgets/product.dart
+++ lib/widgets/product.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:katarasa/models/product_models.dart';
+import 'package:katarasa/models/products/products_request.dart';
 import 'package:katarasa/utils/constant.dart';
-import 'package:katarasa/utils/extension.dart';
+import 'package:katarasa/widgets/general/image.dart';
 
 class Product extends StatelessWidget {
-  final ProductModels products;
+  // final ProductModels products;
+  final ProductRequest products;
 
   const Product({
     Key? key,
@@ -13,7 +14,7 @@ class Product extends StatelessWidget {
 
   Widget _priceTag() {
     Widget price = Text(
-      "Rp ${products.price.toRupiah()}",
+      products.price,
       textAlign: TextAlign.left,
       maxLines: 1,
       style:
@@ -22,9 +23,9 @@ class Product extends StatelessWidget {
 
     Widget discount = Container();
 
-    if (products.discount != null) {
+    if (products.isDiscount != false) {
       price = Text(
-        "Rp ${products.price.toRupiah()}",
+        products.price,
         textAlign: TextAlign.left,
         maxLines: 1,
         style: const TextStyle(
@@ -33,7 +34,7 @@ class Product extends StatelessWidget {
         ),
       );
       discount = Text(
-        "Rp ${products.discount!.toRupiah()}",
+        products.discount,
         textAlign: TextAlign.center,
         maxLines: 1,
         style: const TextStyle(
@@ -45,7 +46,7 @@ class Product extends StatelessWidget {
 
     return Container(
       margin: const EdgeInsets.all(4),
-      child: products.discount == null
+      child: products.isDiscount == false
           ? Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
@@ -65,40 +66,40 @@ class Product extends StatelessWidget {
     );
   }
 
-  Widget _promo() {
-    if (products.discount == null) return Container();
+  // Widget _promo() {
+  //   if (products.discount == null) return Container();
 
-    return Column(
-      children: [
-        Container(
-          // margin: EdgeInsets.all(8),
-          padding: const EdgeInsets.all(8),
-          decoration: BoxDecoration(
-            color: ColorUI.WHITE,
-            borderRadius: const BorderRadius.only(
-              topLeft: Radius.circular(
-                10,
-              ),
-              topRight: Radius.circular(
-                BorderUI.RADIUS_ROUNDED,
-              ),
-              bottomRight: Radius.circular(
-                BorderUI.RADIUS_ROUNDED,
-              ),
-            ),
-            gradient: gradientColor(),
-          ),
-          child: const Text(
-            "Promo",
-            maxLines: 2,
-            style: TextStyle(
-              color: ColorUI.WHITE,
-            ),
-          ),
-        ),
-      ],
-    );
-  }
+  //   return Column(
+  //     children: [
+  //       Container(
+  //         // margin: EdgeInsets.all(8),
+  //         padding: const EdgeInsets.all(8),
+  //         decoration: BoxDecoration(
+  //           color: ColorUI.WHITE,
+  //           borderRadius: const BorderRadius.only(
+  //             topLeft: Radius.circular(
+  //               10,
+  //             ),
+  //             topRight: Radius.circular(
+  //               BorderUI.RADIUS_ROUNDED,
+  //             ),
+  //             bottomRight: Radius.circular(
+  //               BorderUI.RADIUS_ROUNDED,
+  //             ),
+  //           ),
+  //           gradient: gradientColor(),
+  //         ),
+  //         child: const Text(
+  //           "Promo",
+  //           maxLines: 2,
+  //           style: TextStyle(
+  //             color: ColorUI.WHITE,
+  //           ),
+  //         ),
+  //       ),
+  //     ],
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -127,55 +128,115 @@ class Product extends StatelessWidget {
                 borderRadius: BorderRadius.circular(BorderUI.RADIUS_CIRCULAR),
                 child: Stack(
                   children: [
-                    // StdImage(
-                    //   imageUrl: products.image,
-                    // ),
-                    Image.asset(
-                      products.image,
-                      fit: BoxFit.cover,
-                      width: MediaQuery.of(context).size.width * .200,
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        StdImage(
+                            imageUrl: products.image,
+                            fit: BoxFit.cover,
+                            width: MediaQuery.of(context).size.width * .200,
+                            height: MediaQuery.of(context).size.height * .100),
+                        const SizedBox(height: 5),
+                        Row(
+                          children: [
+                            const Icon(Icons.star,
+                                size: 24, color: Colors.yellow),
+                            Text(
+                              products.rating,
+                              style: BLACK_TEXT_STYLE.copyWith(
+                                  fontSize: 18,
+                                  fontWeight: FontUI.WEIGHT_SEMI_BOLD),
+                            )
+                          ],
+                        )
+                      ],
                     ),
+                    // Image.asset(
+                    //   products.image,
+                    //   fit: BoxFit.cover,
+                    //   width: MediaQuery.of(context).size.width * .200,
+                    // ),
                   ],
                 ),
               ),
               Flexible(
                 child: Container(
-                  margin: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+                  margin: const EdgeInsets.fromLTRB(10, 0, 10, 0),
                   child: Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        products.title,
+                        products.name,
                         textAlign: TextAlign.left,
                         style: BLACK_TEXT_STYLE.copyWith(
-                            fontWeight: FontUI.WEIGHT_BOLD),
+                            fontSize: 18, fontWeight: FontUI.WEIGHT_BOLD),
                         maxLines: 2,
                       ),
                       const SizedBox(height: 5),
                       Text(
-                        products.ingredient,
+                        products.location,
                         textAlign: TextAlign.left,
                         style: BLACK_TEXT_STYLE.copyWith(
                             color: ColorUI.BLACK.withOpacity(.60),
                             fontWeight: FontUI.WEIGHT_LIGHT),
                         maxLines: 2,
                       ),
+                      const SizedBox(height: 5),
+                      products.freeOngkir == 'Y'
+                          ? Row(
+                              children: [
+                                Image.asset("assets/icons/free-delivery.png",
+                                    fit: BoxFit.cover,
+                                    width: MediaQuery.of(context).size.width *
+                                        .050,
+                                    height: MediaQuery.of(context).size.height *
+                                        .025),
+                                const SizedBox(width: 7),
+                                Text(
+                                  "Free Ongkir",
+                                  textAlign: TextAlign.left,
+                                  style: BLACK_TEXT_STYLE.copyWith(
+                                      color: ColorUI.BLACK.withOpacity(.60),
+                                      fontWeight: FontUI.WEIGHT_LIGHT),
+                                  maxLines: 2,
+                                )
+                              ],
+                            )
+                          : const SizedBox()
+
+                      // Text(
+                      //   products.title,
+                      //   textAlign: TextAlign.left,
+                      //   style: BLACK_TEXT_STYLE.copyWith(
+                      //       fontWeight: FontUI.WEIGHT_BOLD),
+                      //   maxLines: 2,
+                      // ),
+                      // const SizedBox(height: 5),
+                      // Text(
+                      //   products.ingredient,
+                      //   textAlign: TextAlign.left,
+                      //   style: BLACK_TEXT_STYLE.copyWith(
+                      //       color: ColorUI.BLACK.withOpacity(.60),
+                      //       fontWeight: FontUI.WEIGHT_LIGHT),
+                      //   maxLines: 2,
+                      // ),
                     ],
                   ),
                 ),
               ),
-              _priceTag(),
+              _priceTag()
             ],
           ),
         ),
-        _promo(),
+        // _promo(),
       ],
     );
 
     return InkWell(
       onTap: () {
-        Navigator.pushNamed(context, '/detail-product', arguments: products.id);
-        debugPrint("ini product id nya => ${products.id}");
+        Navigator.pushNamed(context, '/product-detail',
+            arguments: products.slug);
+        debugPrint("ini product slug nya => ${products.slug}");
       },
       child: content,
     );
