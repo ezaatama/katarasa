fbdbd6d6b0daca34a33b17fdc9276e756bf502aa
ezaatama <ezatama33@gmail.com>
2023-08-31 10:10:43 +0700

    choice chip

---
+++ lib/widgets/choice_chip.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:katarasa/utils/constant.dart';
+
+class CategoryType extends StatelessWidget {
+  const CategoryType(
+      {super.key,
+      required this.title,
+      required this.selected,
+      required this.cb});
+
+  final String title;
+  final bool selected;
+  final VoidCallback cb;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(right: 8),
+      child: InkWell(
+        onTap: cb,
+        child: Container(
+          padding: const EdgeInsets.all(8),
+          decoration: BoxDecoration(
+            color: selected ? ColorUI.BACKGROUND_COLOR : ColorUI.WHITE,
+            border: selected ? null : Border.all(color: ColorUI.BLACK),
+            borderRadius: const BorderRadius.all(
+              Radius.circular(
+                BorderUI.RADIUS_ROUNDED,
+              ),
+            ),
+            gradient: selected ? gradientColor() : null,
+          ),
+          child: Text(
+            title,
+            maxLines: 2,
+            style: TextStyle(
+              color: selected ? ColorUI.WHITE : ColorUI.BLACK,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
