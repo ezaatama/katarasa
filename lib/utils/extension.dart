be7021f09207c2d0829ea5b679333343e6cd5425
ezaatama <ezatama33@gmail.com>
2023-09-04 11:48:11 +0700

    ext validation email and phone number

--- lib/utils/extension.dart
+++ lib/utils/extension.dart
@@ -13,6 +13,21 @@ extension ExtendedNum on num {
   }
 }
 
+//email validator
+bool isValidEmail(String value) {
+  String pattern =
+      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+  RegExp regExp = RegExp(pattern);
+  return regExp.hasMatch(value);
+}
+
+//phone number validator
+bool isValidPhoneNumber(String value) {
+  String pattern = r'^[0-9]{9,}$';
+  RegExp regExp = RegExp(pattern);
+  return regExp.hasMatch(value);
+}
+
 void callShowSnackbar(BuildContext context, String msg) {
   final snackBar = SnackBar(content: Text(msg));
 
