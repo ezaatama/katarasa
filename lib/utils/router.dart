2891c1aec1dd7015e72c128b2ab94b4628abb45b
ezaatama <ezatama33@gmail.com>
2023-09-06 11:14:02 +0700

    update router

--- lib/utils/router.dart
+++ lib/utils/router.dart
@@ -6,7 +6,11 @@ import 'package:katarasa/screens/cart/cart_screen.dart';
 import 'package:katarasa/screens/cart/payment_method_screen.dart';
 import 'package:katarasa/screens/product/detail_product_screen.dart';
 import 'package:katarasa/screens/product/product_detail_screen.dart';
+import 'package:katarasa/screens/profile/about_us_screen.dart';
+import 'package:katarasa/screens/profile/all_order_screen.dart';
+import 'package:katarasa/screens/profile/detail_oder_screen.dart';
 import 'package:katarasa/screens/profile/edit_profile_screen.dart';
+import 'package:katarasa/screens/profile/update_password_screen.dart';
 import 'package:katarasa/screens/splash/splashscreen.dart';
 
 class AppRouter {
@@ -20,8 +24,16 @@ class AppRouter {
         return MaterialPageRoute(builder: (_) => const RegisterScreen());
       case '/home':
         return MaterialPageRoute(builder: (_) => const BottomNavScreen());
+      case '/about-us':
+        return MaterialPageRoute(builder: (_) => const AboutUsScreen());
       case '/edit-profile':
         return MaterialPageRoute(builder: (_) => const EditProfileScreen());
+      case '/all-order':
+        return MaterialPageRoute(builder: (_) => const AllOrderScreen());
+      case '/order-detail':
+        return MaterialPageRoute(builder: (_) => const DetailOrderScreen());
+      case '/edit-password':
+        return MaterialPageRoute(builder: (_) => const UpdatePasswordScreen());
       case '/detail-product':
         return MaterialPageRoute(
             builder: (_) =>
