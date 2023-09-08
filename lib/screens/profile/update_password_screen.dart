d84a8a1287d44ce1b699056f468c81d733c0f9f7
ezaatama <ezatama33@gmail.com>
2023-09-08 08:28:25 +0700

    update password profile screen

--- lib/screens/profile/update_password_screen.dart
+++ lib/screens/profile/update_password_screen.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:katarasa/data/profile/ubah_password/ubah_password_cubit.dart';
+import 'package:katarasa/models/profile/ubah_password/ubah_password_request.dart';
 import 'package:katarasa/utils/constant.dart';
+import 'package:katarasa/widgets/button/loading_button.dart';
 import 'package:katarasa/widgets/button/primary_button.dart';
 import 'package:katarasa/widgets/customize_text_field.dart';
 import 'package:katarasa/widgets/general/icon_suffix.dart';
+import 'package:katarasa/widgets/general/toast_comp.dart';
 
 class UpdatePasswordScreen extends StatefulWidget {
   const UpdatePasswordScreen({super.key});
@@ -13,6 +18,7 @@ class UpdatePasswordScreen extends StatefulWidget {
 
 class _UpdatePasswordScreenState extends State<UpdatePasswordScreen> {
   bool _isObscure = false;
+  final _formKey = GlobalKey<FormState>();
   final TextEditingController _passwordController = TextEditingController();
   @override
   Widget build(BuildContext context) {
@@ -37,35 +43,69 @@ class _UpdatePasswordScreenState extends State<UpdatePasswordScreen> {
           child: SingleChildScrollView(
         child: Container(
           margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 20),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              CustomTextField(
-                  controller: _passwordController,
-                  hintText: "Password Baru",
-                  textInputAction: TextInputAction.done,
-                  keyboardType: TextInputType.visiblePassword,
-                  obscureText: !_isObscure,
-                  validator: (value) {
-                    if (value!.isEmpty) {
-                      return 'Password tidak boleh kosong!';
-                    }
-                    return null;
-                  },
-                  suffixIcon: IconSuffixButton(
-                    isObscure: _isObscure,
-                    onPressed: () {
-                      setState(
-                        () {
-                          _isObscure = !_isObscure;
-                        },
-                      );
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                CustomTextField(
+                    controller: _passwordController,
+                    hintText: "Password Baru",
+                    textInputAction: TextInputAction.done,
+                    keyboardType: TextInputType.visiblePassword,
+                    obscureText: !_isObscure,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return 'Password tidak boleh kosong!';
+                      }
+                      return null;
                     },
-                  ),
-                  onFieldSubmitted: (value) {}),
-              const SizedBox(height: 30),
-              PrimaryButton(text: "Simpan", onPressed: () async {})
-            ],
+                    suffixIcon: IconSuffixButton(
+                      isObscure: _isObscure,
+                      onPressed: () {
+                        setState(
+                          () {
+                            _isObscure = !_isObscure;
+                          },
+                        );
+                      },
+                    ),
+                    onFieldSubmitted: (value) {}),
+                const SizedBox(height: 30),
+                BlocConsumer<UbahPasswordCubit, UbahPasswordState>(
+                  listener: (context, state) {
+                    if (state is UbahPasswordSuccess) {
+                      showToast(
+                          text: state.updatePassSuccess,
+                          state: ToastStates.SUCCESS);
+                      Navigator.pushReplacementNamed(context, '/home');
+                    } else if (state is UbahPasswordError) {
+                      showToast(
+                          text: state.errUpdatePass, state: ToastStates.ERROR);
+                    }
+                  },
+                  builder: (context, state) {
+                    var cubit = context.read<UbahPasswordCubit>();
+                    return cubit.isUpdatePass
+                        ? LoadingButton(onPressed: () {
+                            debugPrint('Loading response ubah password');
+                          })
+                        : PrimaryButton(
+                            text: "Simpan",
+                            onPressed: () async {
+                              if (_formKey.currentState!.validate()) {
+                                UbahPasswordRequest payload =
+                                    UbahPasswordRequest(
+                                        passwordBaru: _passwordController.text);
+                                context
+                                    .read<UbahPasswordCubit>()
+                                    .updatePassProfile(context, payload);
+                              }
+                            });
+                  },
+                )
+              ],
+            ),
           ),
         ),
       )),
