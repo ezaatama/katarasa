a6c5a83438b3c8ce26a41f504318cccfff2501ba
ezaatama <ezatama33@gmail.com>
2023-09-08 08:28:44 +0700

    update endpoints

--- lib/utils/endpoints.dart
+++ lib/utils/endpoints.dart
@@ -1,6 +1,11 @@
+// ignore_for_file: constant_identifier_names
+
 const LOGIN = 'auth/login';
 const REGISTER = 'auth/register';
 const FORGOT_PASSWORD = 'auth/lupa-password';
+const CHANGE_PASSWORD = 'profile/update-password';
+const DATA_PROFILE = 'profile/data-profile';
+const EDIT_DATA_PROFILE = 'profile/edit-profile';
 const CATEGORIES = 'product/get-kategori';
 const ALL_PRODUCTS = 'product/get-product';
 const PRODUCT_DETAIL = 'product/get-product-detail';
