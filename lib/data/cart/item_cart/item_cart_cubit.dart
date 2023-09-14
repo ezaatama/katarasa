d23c58cf659da438b1d6ff40677a37e7a5ad416d
ezaatama <ezatama33@gmail.com>
2023-09-14 08:08:35 +0700

    update item cart cubit

--- lib/data/cart/item_cart/item_cart_cubit.dart
+++ lib/data/cart/item_cart/item_cart_cubit.dart
@@ -1,6 +1,7 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
+import 'package:katarasa/models/cart/all_cart_request.dart';
 import 'package:katarasa/models/cart/cart_item_request.dart';
 import 'package:katarasa/utils/base_response.dart';
 import 'package:katarasa/utils/constant.dart';
@@ -14,6 +15,8 @@ part 'item_cart_state.dart';
 class ItemCartCubit extends Cubit<ItemCartState> {
   ItemCartCubit() : super(ItemCartInitial());
 
+  List<CartItemRequest> cartItems = [];
+
   Future<void> addToCart(CartItemRequest payload, BuildContext context) async {
     Future<ObjResponse> res =
         callNetwork(ADD_TO_CART, body: payload.toMap(), mode: POST_METHOD);
@@ -24,7 +27,23 @@ class ItemCartCubit extends Cubit<ItemCartState> {
           debugPrint(
               'ini value response message + ${value.response['status']['message']}');
 
-          emit(ItemCartUpdated(value.response['status']['message']));
+          int index =
+              cartItems.indexWhere((e) => e.productId == payload.productId);
+          debugPrint('add to cart index == $index');
+
+          if (index != -1) {
+            cartItems[index] = CartItemRequest(
+                productId: payload.productId,
+                variantId: payload.variantId,
+                quantity: cartItems[index].quantity + 1);
+          } else {
+            cartItems.add(CartItemRequest(
+                productId: payload.productId,
+                variantId: payload.variantId,
+                quantity: 1));
+          }
+
+          emit(ItemCartUpdated(cartItems, value.response['status']['message']));
         } else {
           debugPrint(value.errresponse.errors);
           emit(ItemCartError(value.errresponse.errors));
@@ -38,6 +57,138 @@ class ItemCartCubit extends Cubit<ItemCartState> {
       } else {
         debugPrint('error response is ${e.toString()}');
         emit(ItemCartError(e.toString()));
+      }
+    });
+  }
+
+  Future<void> incrementCartItem(
+      CartItemRequest payload, BuildContext context) async {
+    Future<ObjResponse> res =
+        callNetwork(UPDATE_QTY_CART, body: payload.toMap(), mode: PUT_METHOD);
+
+    await res.then((value) {
+      if (value.success == true) {
+        if (value.success == true) {
+          debugPrint(
+              'ini value response message + ${value.response['status']['message']}');
+
+          int index = cartItems.indexWhere((e) {
+            debugPrint('element product id index == ${e.productId}');
+            debugPrint('product id index == ${payload.productId}');
+
+            return e.productId == payload.productId;
+          });
+          debugPrint('increment index == $index');
+          if (index != -1) {
+            cartItems[index].incrementQuantity();
+          }
+
+          emit(ItemCartUpdated(cartItems, value.response['status']['message']));
+        } else {
+          debugPrint(value.errresponse.errors);
+          emit(ItemCartError(value.errresponse.errors));
+        }
+      }
+    }).catchError((e) {
+      if (e is ConnectionProblemException || e is TimeoutException) {
+        callShowSnackbar(context, e.toString());
+      } else if (e is InternalServerException) {
+        callShowSnackbar(context, e.toString());
+      } else {
+        debugPrint('error response is ${e.toString()}');
+        emit(ItemCartError(e.toString()));
+      }
+    });
+  }
+
+  Future<void> decrementCartItem(
+      CartItemRequest payload, BuildContext context) async {
+    Future<ObjResponse> res =
+        callNetwork(UPDATE_QTY_CART, body: payload.toMap(), mode: PUT_METHOD);
+
+    await res.then((value) {
+      if (value.success == true) {
+        if (value.success == true) {
+          debugPrint(
+              'ini value response message + ${value.response['status']['message']}');
+
+          int index =
+              cartItems.indexWhere((e) => e.productId == payload.productId);
+          debugPrint('increment index == $index');
+          if (index != -1) {
+            cartItems[index].decrementQuantity();
+          }
+
+          emit(ItemCartUpdated(cartItems, value.response['status']['message']));
+        } else {
+          debugPrint(value.errresponse.errors);
+          emit(ItemCartError(value.errresponse.errors));
+        }
+      }
+    }).catchError((e) {
+      if (e is ConnectionProblemException || e is TimeoutException) {
+        callShowSnackbar(context, e.toString());
+      } else if (e is InternalServerException) {
+        callShowSnackbar(context, e.toString());
+      } else {
+        debugPrint('error response is ${e.toString()}');
+        emit(ItemCartError(e.toString()));
+      }
+    });
+  }
+
+  Future<void> setSelectItem(BuildContext context, String cartId) async {
+    Future<ObjResponse> res =
+        callNetwork(SELECTED_CART, body: {'cart_id': cartId}, mode: PUT_METHOD);
+
+    await res.then((value) {
+      if (value.success == true) {
+        if (value.success == true) {
+          debugPrint(
+              'ini value response message + ${value.response['status']['message']}');
+
+          emit(SelectedItemSuccess(value.response['status']['message']));
+        } else {
+          debugPrint(value.errresponse.errors);
+          emit(SelectedItemError(value.errresponse.errors));
+        }
+      }
+    }).catchError((e) {
+      if (e is ConnectionProblemException || e is TimeoutException) {
+        callShowSnackbar(context, e.toString());
+      } else if (e is InternalServerException) {
+        callShowSnackbar(context, e.toString());
+      } else {
+        debugPrint('error response is ${e.toString()}');
+        emit(SelectedItemError(e.toString()));
+      }
+    });
+  }
+
+  Future<void> deleteItemCart(BuildContext context, String cartId) async {
+    Future<ObjResponse> res = callNetwork("${DELETE_CART}cart_id=$cartId",
+        body: {'cart_id': cartId}, mode: POST_METHOD);
+
+    await res.then((value) {
+      if (value.success == true) {
+        if (value.success == true) {
+          debugPrint(
+              'ini value response message + ${value.response['status']['message']}');
+
+          emit(DeleteItemSuccess(value.response['status']['message']));
+        } else {
+          debugPrint(value.errresponse.errors);
+          emit(DeleteItemError(value.errresponse.errors));
+        }
+      }
+    }).catchError((e) {
+      if (e is ConnectionProblemException || e is TimeoutException) {
+        callShowSnackbar(context, e.toString());
+      } else if (e is InternalServerException) {
+        callShowSnackbar(context, e.toString());
+      } else {
+        debugPrint('error response is ${e.toString()}');
+        emit(DeleteItemError(e.toString()));
       }
     });
   }
--- lib/data/cart/item_cart/item_cart_state.dart
+++ lib/data/cart/item_cart/item_cart_state.dart
@@ -10,13 +10,38 @@ sealed class ItemCartState extends Equatable {
 final class ItemCartInitial extends ItemCartState {}
 
 final class ItemCartUpdated extends ItemCartState {
-  final String itemCartUpdated;
+  final List<CartItemRequest> itemCartUpdated;
+  final String cartUpdated;
 
-  const ItemCartUpdated(this.itemCartUpdated);
+  const ItemCartUpdated(this.itemCartUpdated, this.cartUpdated);
 }
 
 final class ItemCartError extends ItemCartState {
   final String errItemCart;
 
   const ItemCartError(this.errItemCart);
+}
+
+final class SelectedItemSuccess extends ItemCartState {
+  final String selectSuccess;
+
+  const SelectedItemSuccess(this.selectSuccess);
+}
+
+final class SelectedItemError extends ItemCartState {
+  final String selectError;
+
+  const SelectedItemError(this.selectError);
+}
+
+final class DeleteItemSuccess extends ItemCartState {
+  final String deleteSuccess;
+
+  const DeleteItemSuccess(this.deleteSuccess);
+}
+
+final class DeleteItemError extends ItemCartState {
+  final String deleteError;
+
+  const DeleteItemError(this.deleteError);
 }
