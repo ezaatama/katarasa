7be81b7b4c9df8971523d5b4982a7089c63c8b59
ezaatama <ezatama33@gmail.com>
2023-10-02 09:49:05 +0700

    update all order

--- lib/screens/profile/all_order_screen.dart
+++ lib/screens/profile/all_order_screen.dart
@@ -1,21 +1,80 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_offline/flutter_offline.dart';
+import 'package:katarasa/data/order/data_order_cubit.dart';
 import 'package:katarasa/utils/constant.dart';
+import 'package:katarasa/widgets/order/card_all_order.dart';
+import 'package:shimmer/shimmer.dart';
 
-class AllOrderScreen extends StatelessWidget {
+class AllOrderScreen extends StatefulWidget {
   const AllOrderScreen({super.key});
+
+  @override
+  State<AllOrderScreen> createState() => _AllOrderScreenState();
+}
+
+class _AllOrderScreenState extends State<AllOrderScreen> {
+  @override
+  void initState() {
+    super.initState();
+    context.read<DataOrderCubit>().getAllOrder(context);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: OfflineBuilder(
+          connectivityBuilder: (
+            BuildContext context,
+            ConnectivityResult connectivity,
+            Widget child,
+          ) {
+            final bool connected = connectivity != ConnectivityResult.none;
+            return Stack(
+              fit: StackFit.expand,
+              children: [
+                _bodyContent(),
+                Positioned(
+                  height: 24.0,
+                  bottom: 0.0,
+                  left: 0.0,
+                  right: 0.0,
+                  child: connected
+                      ? const SizedBox()
+                      : Container(
+                          color: const Color(0xFFEE4400),
+                          child: Center(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text("Periksa Kembali Jaringan Anda",
+                                    style: WHITE_TEXT_STYLE.copyWith(
+                                        fontSize: 14,
+                                        fontWeight: FontUI.WEIGHT_SEMI_BOLD)),
+                              ],
+                            ),
+                          ),
+                        ),
+                ),
+              ],
+            );
+          },
+          child: _bodyContent()),
+    );
+  }
+
+  Widget _bodyContent() {
+    return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        // leading: IconButton(
-        //     icon: const Icon(Icons.arrow_back_rounded,
-        //         size: 24, color: ColorUI.BLACK),
-        //     onPressed: () {
-        //       Navigator.pop(context);
-        //     }),
+        leading: IconButton(
+            icon: const Icon(Icons.arrow_back_rounded,
+                size: 24, color: ColorUI.BLACK),
+            onPressed: () {
+              Navigator.pop(context);
+            }),
         title: Text(
           "Semua Pesanan",
           style: BLACK_TEXT_STYLE.copyWith(
@@ -24,16 +83,99 @@ class AllOrderScreen extends StatelessWidget {
         centerTitle: true,
       ),
       body: SafeArea(
-          child: SingleChildScrollView(
-        child: Container(
-          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-          child: Column(
-            children: [
-              // CardAllOrder()
-            ],
+          child: Container(
+        width: MediaQuery.of(context).size.width,
+        height: MediaQuery.of(context).size.height,
+        margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 15),
+        child: SingleChildScrollView(
+          child: BlocBuilder<DataOrderCubit, DataOrderState>(
+            builder: (context, state) {
+              if (state is DataOrderLoading) {
+                _shimmerContent();
+              } else if (state is DataOrderEmpty) {
+                return const Center(child: Text("Data Order Kosong"));
+              } else if (state is DataOrderLoaded) {
+                return ListView.builder(
+                    shrinkWrap: true,
+                    primary: false,
+                    itemCount: state.dataOrderLoaded.items.length,
+                    itemBuilder: (context, index) {
+                      final item = state.dataOrderLoaded.items[index];
+                      final invoice = item.items[index];
+                      final store = item.items[index];
+                      final product = item.items[index].products[index];
+                      return CardAllOrder(
+                          item: item,
+                          invoice: invoice,
+                          store: store,
+                          productName: product,
+                          productSubTotal: item);
+                    });
+              }
+              return const SizedBox();
+            },
           ),
         ),
       )),
+    );
+  }
+
+  Widget _shimmerContent() {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 16),
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      child: Shimmer.fromColors(
+        baseColor: ColorUI.SHIMMER_BASE,
+        highlightColor: ColorUI.SHIMMER_HIGHLIGHT,
+        child: ListView.builder(
+          itemBuilder: (_, __) => Padding(
+            padding: const EdgeInsets.only(bottom: 8.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Container(
+                  width: 48.0,
+                  height: 48.0,
+                  color: Colors.white,
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 8.0),
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Container(
+                        width: double.infinity,
+                        height: 8.0,
+                        color: Colors.white,
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 2.0),
+                      ),
+                      Container(
+                        width: double.infinity,
+                        height: 8.0,
+                        color: Colors.white,
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 2.0),
+                      ),
+                      Container(
+                        width: 40.0,
+                        height: 8.0,
+                        color: Colors.white,
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          ),
+          itemCount: 6,
+        ),
+      ),
     );
   }
 }
