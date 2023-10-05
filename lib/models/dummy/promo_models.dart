dd7ed547bf4fe9cd134b77b6ee08e59e07d840dd
ezaatama <ezatama33@gmail.com>
2023-10-05 10:44:36 +0700

    update promo models

--- lib/models/dummy/promo_models.dart
+++ lib/models/dummy/promo_models.dart
@@ -5,7 +5,7 @@ class PromoModels {
 }
 
 List<PromoModels> promoData = [
-  PromoModels(image: 'assets/images/banner_promo.png'),
-  PromoModels(image: 'assets/images/banner_promo.png'),
+  PromoModels(image: 'assets/images/banner_promo1.png'),
+  PromoModels(image: 'assets/images/banner_promo2.png'),
   PromoModels(image: 'assets/images/banner_promo.png'),
 ];
