efcd6a9b24534ccc75221619639d63f5fac3dbe4
ezaatama <ezatama33@gmail.com>
2023-10-12 14:47:21 +0700

    update filter status

---
+++ lib/models/filter/filter_status.dart
@@ -0,0 +1,37 @@
+List<dynamic> orderFilter = [
+  {
+    "id": 1,
+    "title": 'Belum Dibayar',
+    "value": 'notPaid',
+  },
+  {
+    "id": 2,
+    "title": 'Menunggu Konfirmasi',
+    "value": 'waitConfirm',
+  },
+  {
+    "id": 3,
+    "title": 'Disiapkan',
+    "value": 'prepared',
+  },
+  {
+    "id": 4,
+    "title": 'Dikirim',
+    "value": 'sent',
+  },
+  {
+    "id": 5,
+    "title": 'Selesai',
+    "value": 'done',
+  },
+  {
+    "id": 6,
+    "title": 'Dibatalkan',
+    "value": 'canceled',
+  },
+  {
+    "id": 7,
+    "title": 'Pengembalian',
+    "value": 'refund',
+  },
+];
