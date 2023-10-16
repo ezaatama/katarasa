010b53fb6ed2cbdb86f6545c4fadb8a4548457d4
ezaatama <ezatama33@gmail.com>
2023-10-16 10:18:34 +0700

    update payment cubit

--- lib/data/payment/payment_token/payment_cubit.dart
+++ lib/data/payment/payment_token/payment_cubit.dart
@@ -2,6 +2,7 @@ import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:katarasa/models/payment/payment_snap_before_token.dart';
+import 'package:katarasa/models/payment/payment_snap_update.dart';
 import 'package:katarasa/utils/base_response.dart';
 import 'package:katarasa/utils/constant.dart';
 import 'package:katarasa/utils/endpoints.dart';
@@ -40,6 +41,33 @@ class PaymentCubit extends Cubit<PaymentState> {
       } else {
         debugPrint('error response is ${e.toString()}');
         emit(PaymentSnapError(e.toString()));
+      }
+    });
+  }
+
+  Future<void> postPaySnapUpdate(
+      BuildContext context, PaySnapUpdateData payload) async {
+    emit(PaymentSnapUpdateLoading());
+
+    Future<ObjResponse> res = callNetwork(PAYMENT_SNAP_TOKEN_UPDATE,
+        mode: POST_METHOD, body: payload.toJson());
+
+    await res.then((value) {
+      if (value.success == true) {
+        PaySnapUpdate response = PaySnapUpdate.fromJson(value.response['data']);
+        emit(PaymentSnapUpdateSuccess(response));
+      } else {
+        debugPrint(value.errresponse.errors);
+        emit(PaymentSnapUpdateError(value.errresponse.errors));
+      }
+    }).catchError((e) {
+      if (e is ConnectionProblemException || e is TimeoutException) {
+        callShowSnackbar(context, e.toString());
+      } else if (e is InternalServerException) {
+        callShowSnackbar(context, e.toString());
+      } else {
+        debugPrint('error response is ${e.toString()}');
+        emit(PaymentSnapUpdateError(e.toString()));
       }
     });
   }
--- lib/data/payment/payment_token/payment_state.dart
+++ lib/data/payment/payment_token/payment_state.dart
@@ -11,14 +11,28 @@ final class PaymentInitial extends PaymentState {}
 
 final class PaymentSnapLoading extends PaymentState {}
 
+final class PaymentSnapUpdateLoading extends PaymentState {}
+
 final class PaymentSnapSuccess extends PaymentState {
   final PaySnapResponse paySnapSuccess;
 
   const PaymentSnapSuccess(this.paySnapSuccess);
 }
 
+final class PaymentSnapUpdateSuccess extends PaymentState {
+  final PaySnapUpdate paySnapUpdSuccess;
+
+  const PaymentSnapUpdateSuccess(this.paySnapUpdSuccess);
+}
+
 final class PaymentSnapError extends PaymentState {
   final String errPaySnap;
 
   const PaymentSnapError(this.errPaySnap);
+}
+
+final class PaymentSnapUpdateError extends PaymentState {
+  final String errUpdPaySnap;
+
+  const PaymentSnapUpdateError(this.errUpdPaySnap);
 }
